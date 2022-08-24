836c57df2c47d0fabe182a08cff9f4a2c1d36bc2
Khalidiqnaibi <khalidqnabi@gamil.com>
2022-08-25 00:41:41 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -7,14 +7,35 @@ import Acheve from './componants/pages/achevments';
 import Contact from './componants/pages/contact-us';
 import Nav from './componants/nav/nav';
 import Footer from './componants/footer/footer';
+//"proxy":"http://localhost:7000",
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAT6HVKeZG5UCqoLylO56Xv6YKgUEPB2Y4",
+  authDomain: "personal-website-b86de.firebaseapp.com",
+  projectId: "personal-website-b86de",
+  storageBucket: "personal-website-b86de.appspot.com",
+  messagingSenderId: "12000173177",
+  appId: "1:12000173177:web:f2d17ef9acf350f7e148a8",
+  measurementId: "G-55WGS2RYJD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App(){
    return(
      <Router>
        <Nav/>
-       <Routes>   
+       <Routes> 
          <Route path='/Contact_us' element={<Contact/>}/>
          <Route path="/achievements" element={<Acheve/>} />   
          <Route path="/about" element={<About />}/> 
---
+++ src/componants/nav/burgermnue/burgermnue.js
@@ -0,0 +1,33 @@
+import React,{useState} from "react";
+import { Link } from "react-router-dom";
+
+export default function Burgr(){
+  const [Open,setOpen]=useState(false);
+  function ICON(){
+    if (!Open){
+    return(
+      <div className="menu-btn" onClick={()=>setOpen(!Open)}>
+        <img src="./list.svg" alt="menu" className="menu-btn__burger"/>
+      </div>
+    );
+  }else{
+    return(
+      <div className="menu-btn" onClick={()=>setOpen(!Open)}>
+        <img src="./x-lg.svg" alt="xmenu" className="menu-btn__burger"/>
+      </div>
+    )
+  }
+  }
+    return(
+        
+      <div className='burgrm'>
+        <ICON/>
+        {Open&&<div className ='mnav'> 
+            <div className='upm' id="fstmn"><Link id='homem' to='/'><div claasName='nbtn' ><h5>Home</h5></div></Link></div>  
+            <div className='upm' ><Link id='achevmentsm' to='/achievements'><div claasName='nbtn' ><h5>Achievements</h5></div></Link></div> 
+            <div className='upm' ><Link id='aboutm' to='/about'><div claasName='nbtn' ><h5>About</h5></div></Link></div>
+            <div className='upm' id="lastmn" ><Link id='contactm' to="/Contact_us"><div  claasName='nbtn' ><h5>Contact</h5></div></Link></div>           
+        </div> }
+      </div>  
+    )
+}
